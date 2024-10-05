5a114aec0817959eaa492eecc5195ea8a29a40e9
ian <ian.sobrecaray@gmail.com>
2024-10-06 04:39:55 +0800

    improved the log message of auth logs

--- middleware/loggingMiddleware.js
+++ middleware/loggingMiddleware.js
@@ -1,5 +1,13 @@
 module.exports = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    const method = req.method;
+    const url = req.url;
+    const timestamp = new Date().toISOString();
+  
+    // After response is sent
+    res.on('finish', () => {
+      const statusCode = res.statusCode;  // Capture response status code
+      console.log(`[${timestamp}] ${method} request to ${url} - Status: ${statusCode}`);
+    });
     next();
 };
   
