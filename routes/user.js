a1291893082928c33adc2df963d5232de0599c68
ian <ian.sobrecaray@gmail.com>
2024-10-06 02:07:08 +0800

    Finished the login feature and installed jsonwebtoken npm package

--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,7 @@ const userController = require('../controllers/userController');
 // POST /register
 router.post('/register', userController.register);
 
+// POST /login
+router.post('/login', userController.login);
+
 module.exports = router;
