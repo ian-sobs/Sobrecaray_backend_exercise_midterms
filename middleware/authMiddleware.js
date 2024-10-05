d87e6a34975e75551044e3a7e2115b38ce70a478
ian <ian.sobrecaray@gmail.com>
2024-10-06 05:00:26 +0800

    Fixed an issue where the token was invalid

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,11 @@ require('dotenv').config()
 const SECRET_KEY = process.env.SECRET_KEY;
 
 module.exports = (req, res, next) => {
-    const token = req.headers['authorization'];
+    // Get the Authorization header from the request
+    const authHeader = req.headers['authorization'];
+
+    // Check if the header exists and extract the token
+    const token = authHeader && authHeader.split(' ')[1];  // Extract the Bearer token
     if (!token) {
         return res.status(403).json({ message: "No token provided" });
     }
