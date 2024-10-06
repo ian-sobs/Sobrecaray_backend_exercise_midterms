84df4bd36506d6d876072990891cd0abd5f8a55f
ian <ian.sobrecaray@gmail.com>
2024-10-06 15:41:47 +0800

    Started implementing a read and write feature from and to the mock database

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,23 @@
-const users = [];
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join('../data', 'users.json');
+
+let users = [];
+fs.readFile(filePath, 'utf8', (err, contents) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  try {
+    const jsonString = JSON.parse(contents);
+    console.log(jsonString);
+  } catch (jsonError) {
+    console.error('Error parsing JSON');
+  }
+});
+
 
 module.exports = {
   findUserByEmail: (email) => users.find(user => user.email === email),
