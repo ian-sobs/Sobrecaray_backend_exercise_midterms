04a9017413c362b757d0a40a5280c169131da21e
ian <ian.sobrecaray@gmail.com>
2024-10-06 15:58:48 +0800

    Read-and-write feature from and to the mock database is complete

--- models/userModel.js
+++ models/userModel.js
@@ -11,8 +11,8 @@ fs.readFile(filePath, 'utf8', (err, contents) => {
   }
 
   try {
-    const jsonString = JSON.parse(contents);
-    console.log(jsonString);
+    let jsonArr = JSON.parse(contents);
+    users = jsonArr;
   } catch (jsonError) {
     console.error('Error parsing JSON');
   }
@@ -21,7 +21,18 @@ fs.readFile(filePath, 'utf8', (err, contents) => {
 
 module.exports = {
   findUserByEmail: (email) => users.find(user => user.email === email),
-  addUser: (user) => users.push(user),
+  //addUser: (user) => users.push(user),
+  addUser: function(user){
+    users.push(user)
+    const jsonStringArr = JSON.stringify(users)
+    fs.writeFile(filePath, jsonStringArr, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('New user added to database');
+      }
+    });
+  },
   findUserById: (id) => users.find(user => user.id === id),
   getUsers: () => users
 };
